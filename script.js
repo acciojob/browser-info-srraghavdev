e81bf88e68eb1881a9ecc5146ab25f3c06fd4353
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-21 22:44:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 //your JS code here. If required.
-const nav= document.getElementsById("df")
+const nav= document.getElementById("df")
 nav.innerText=`You are using ${window.navigator.appName} version ${window.navigator.appVersion}`
